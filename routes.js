b11490a98142ed084b84bf3325f318dbe312e1c3
stolinski <scotttolinski@gmail.com>
2015-09-26 14:32:03 -0600

    adds latest projects

--- routes.js
+++ routes.js
@@ -1,31 +1,30 @@
+/////////////////////////////////
+// Routing Configuration
+/////////////////////////////////
 Router.configure({
     layoutTemplate: 'Layout'
 });
 
 Router.route('/', function() {
     this.render('home');
-    this.render('Nav', {to: 'nav'});
 }, {
     name: 'home'
 });
 
 Router.route('/about', function() {
     this.render('about');
-    this.render('Nav', {to: 'nav'});
 }, {
     name: 'about'
 });
 
 Router.route('/projects', function() {
     this.render('projects');
-    this.render('Nav', {to: 'nav'});
 }, {
     name: 'projects'
 });
 
 Router.route('/contact', function() {
     this.render('contact');
-    this.render('Nav', {to: 'nav'});
 }, {
     name: 'contact'
 });
--- st2015.js
+++ st2015.js
@@ -1,16 +1,5 @@
+_ = lodash;
+
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
-}
-
-
-if (Meteor.isClient) {
-    Template.Layout.helpers({
-    });
-
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
